e9ff7a521e9dcd3b8a5d590606a6321f4ac63229
Ivan5 <converse_ivan@hotmail.com>
2019-06-28 17:43:42 -0500

    Add firebase and make Header component and styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyCvzjuFkGU731UPt-hH2sjVuzjXxE8z7gw",
+  authDomain: "react-notes-app-db593.firebaseapp.com",
+  databaseURL: "https://react-notes-app-db593.firebaseio.com",
+  projectId: "react-notes-app-db593",
+  storageBucket: "",
+  messagingSenderId: "775929808687",
+  appId: "1:775929808687:web:026d31ea223e442e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<App />, document.getElementById("root"));
+
 serviceWorker.unregister();
