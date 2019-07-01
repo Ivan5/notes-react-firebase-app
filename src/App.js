36e307bea9a9742cad44a8dced9ef1027948f22c
Ivan5 <converse_ivan@hotmail.com>
2019-07-01 14:59:22 -0500

    Add a note into firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import * as firebase from "firebase";
 import Header from "./components/Header";
+import NotesForm from "./components/NotesForm";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <NotesForm />
+      </main>
     </div>
   );
 }
